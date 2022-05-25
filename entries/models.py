3896c79ef0f88032a058528627774bc8e02c5c0d
Nacho <nachodoradollamas@hotmail.com>
2022-05-25 14:00:52 +0200

    feat: first model definition

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class EntryModels(models.Model):
+    
+    datetime = models.DateTimeField()
+    concept = models.CharField(max_length=255)
+    amount = models.FloatField()
